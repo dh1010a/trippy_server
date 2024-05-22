78f97bba92ec70009e2cf2271c4ea7fb71523fea
dh1010a <dh1010a@naver.com>
2024-05-22 15:17:15 +0900

    fix: 우분투에서 파일 폴더 생성하도록 수정

--- src/main/java/com/example/server/domain/image/service/OracleImageService.java
+++ src/main/java/com/example/server/domain/image/service/OracleImageService.java
@@ -317,7 +317,14 @@ public class OracleImageService implements ImageService {
 
     // 로컬에 파일 업로드 해서 convert
     private Optional<File> convert(MultipartFile file) throws IOException {
+        String directoryPath = System.getProperty("user.home") + "/rideTogetherDummy/";
+        File directory = new File(directoryPath);
+        if (!directory.exists()) {
+            directory.mkdirs(); // 디렉토리 생성
+        }
+
         File convertFile = new File(System.getProperty("user.home") + "/rideTogetherDummy/" + UUID.randomUUID() +file.getOriginalFilename());
+        System.out.println("System.getProperty = " + System.getProperty("user.home"));
         if (convertFile.createNewFile()) { // 바로 위에서 지정한 경로에 File이 생성됨 (경로가 잘못되었다면 생성 불가능)
             try (FileOutputStream fos = new FileOutputStream(
                     convertFile)) { // FileOutputStream 데이터를 파일에 바이트 스트림으로 저장하기 위함
