b653cc0f639dc335aec7601c2efa35660cf7018a
DevYoung00 <dkdud203@naver.com>
2024-07-01 18:01:00 +0900

    feat: 스프링부트에서 flask 서버로 통신 기능

---
+++ src/main/java/com/example/server/global/config/TemplateProperties.java
@@ -0,0 +1,13 @@
+package com.example.server.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class TemplateProperties {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
