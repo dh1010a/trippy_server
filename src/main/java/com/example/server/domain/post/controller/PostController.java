3cbb27f39599947ff0762843e619061c9d3f5348
DevYoung00 <dkdud203@naver.com>
2024-08-04 21:50:37 +0900

    refactor: PostController refactoring

--- src/main/java/com/example/server/domain/post/controller/PostController.java
+++ src/main/java/com/example/server/domain/post/controller/PostController.java
@@ -1,7 +1,5 @@
 package com.example.server.domain.post.controller;
 
-import com.example.server.domain.member.controller.MemberController;
-import com.example.server.domain.member.dto.MemberRequestDto;
 import com.example.server.domain.post.dto.PostRequestDto;
 import com.example.server.domain.post.model.OrderType;
 import com.example.server.domain.post.model.PostType;
@@ -12,12 +10,9 @@ import com.example.server.global.apiPayload.exception.handler.ErrorHandler;
 import com.example.server.global.util.SecurityUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
 
 @Slf4j
 @RestController
@@ -36,51 +31,45 @@ public class PostController {
     }
 
     @PatchMapping("")
-    public  ApiResponse<?> updatePost(@RequestBody PostRequestDto.UpdatePostRequestDto updatePostRequestDto) {
+    public ApiResponse<?> updatePost(@RequestBody PostRequestDto.UpdatePostRequestDto updatePostRequestDto) {
         String memberId = getLoginMemberId();
         updatePostRequestDto.setMemberId(memberId);
-        log.info("게시물 업데이트 요청 : memberId = {}, postId = {}", memberId,updatePostRequestDto.getId() );
+        log.info("게시물 업데이트 요청 : memberId = {}, postId = {}", memberId, updatePostRequestDto.getId());
         return ApiResponse.onSuccess(postService.updatePost(updatePostRequestDto));
     }
-
 
     @DeleteMapping("/{id}")
     public ApiResponse<?> deletePost(@PathVariable("id") Long postId) {
         String memberId = getLoginMemberId();
-        log.info("게시물 삭제 요청 : memberId = {}, postId = {}", memberId,postId );
-        return ApiResponse.onSuccess(postService.deletePost(postId,memberId));
+        log.info("게시물 삭제 요청 : memberId = {}, postId = {}", memberId, postId);
+        return ApiResponse.onSuccess(postService.deletePost(postId, memberId));
     }
 
     @GetMapping("/info/{id}")
     public ApiResponse<?> getPost(@PathVariable("id") Long postId, HttpServletRequest request, HttpServletResponse response) {
         String loginMemberId = getLoginMemberId();
-        log.info("게시물 조회 요청 : postId = {}, loginMemberId = {}",postId,loginMemberId );
-        return ApiResponse.onSuccess(postService.getPost(postId,loginMemberId,request,response));
+        log.info("게시물 조회 요청 : postId = {}, loginMemberId = {}", postId, loginMemberId);
+        return ApiResponse.onSuccess(postService.getPost(postId, loginMemberId, request, response));
     }
 
     @GetMapping("/all")
     public ApiResponse<?> getAllPost(
             @RequestParam(defaultValue = "0") Integer page,
             @RequestParam(defaultValue = "0") Integer size,
-            @RequestParam(defaultValue = "LATEST") OrderType orderType
-    ) {
+            @RequestParam(defaultValue = "LATEST") OrderType orderType) {
         String loginMemberId = getLoginMemberId();
-        log.info("모든 게시물 조회 요청 : loginMemberId = {}",loginMemberId);
-         return ApiResponse.onSuccess(postService.getAllPost(page,loginMemberId ,size,orderType));
+        log.info("모든 게시물 조회 요청 : loginMemberId = {}", loginMemberId);
+        return ApiResponse.onSuccess(postService.getAllPost(page, loginMemberId, size, orderType));
     }
 
     @GetMapping("/my")
     public ApiResponse<?> getAllLoginMemberPost(
-            @RequestParam(required = false) Integer page,
-            @RequestParam(required = false) Integer size,
-            @RequestParam(required = false) OrderType orderType
-    ) {
-        if(page == null) page = 0;
-        if(size==null) size = 0;
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "0") Integer size,
+            @RequestParam(defaultValue = "LATEST") OrderType orderType) {
         String memberId = getLoginMemberId();
-        if(orderType == null ) orderType = OrderType.LATEST;
-        log.info("로그인 회원 게시물 조회 요청 : memberId = {}", memberId );
-        return ApiResponse.onSuccess(postService.getAllMemberPost(memberId,memberId,page,size,orderType));
+        log.info("로그인 회원 게시물 조회 요청 : memberId = {}", memberId);
+        return ApiResponse.onSuccess(postService.getAllMemberPost(memberId, memberId, page, size, orderType));
     }
 
     @GetMapping("/by-member")
@@ -88,11 +77,10 @@ public class PostController {
             @RequestParam(defaultValue = "0") Integer page,
             @RequestParam(defaultValue = "0") Integer size,
             @RequestParam(defaultValue = "LATEST") OrderType orderType,
-            @RequestParam String memberId
-    ) {
+            @RequestParam String memberId) {
         String loginMemberId = getLoginMemberId();
-        log.info("회원별 게시물 조회 요청 : memberId = {},loginMemberId = {}", memberId ,loginMemberId);
-        return ApiResponse.onSuccess(postService.getAllMemberPost(memberId,loginMemberId,page,size,orderType));
+        log.info("회원별 게시물 조회 요청 : memberId = {}, loginMemberId = {}", memberId, loginMemberId);
+        return ApiResponse.onSuccess(postService.getAllMemberPost(memberId, loginMemberId, page, size, orderType));
     }
 
     @GetMapping("/follow")
@@ -103,40 +91,31 @@ public class PostController {
             @RequestParam PostType postType) {
         String memberId = getLoginMemberId();
         log.info("팔로잉 게시물 조회 요청 : memberId = {}", memberId);
-        if(postType.equals(PostType.POST)) {
-            return ApiResponse.onSuccess(postService.getPostsFromFollowedMembers(memberId, postType, page, size, orderType));
-        }
-        else {
-            return ApiResponse.onSuccess(postService.getOotdsFromFollowedMembers(memberId, postType, page, size, orderType));
-        }
+        return ApiResponse.onSuccess(postService.getPostsFromFollowedMembers(memberId, postType, page, size, orderType));
     }
 
     @GetMapping("/count/all")
     public ApiResponse<?> getTotalCount(@RequestParam PostType type) {
-        log.info("{} 전체 개수 출력", type );
+        log.info("{} 전체 개수 출력", type);
         return ApiResponse.onSuccess(postService.getTotalCount(type));
     }
-    // 로그인 된 멤버
+
     @GetMapping("/count/my")
     public ApiResponse<?> getTotalCountByLoginMember(@RequestParam PostType type) {
         String memberId = getLoginMemberId();
         log.info("{}의 {} 개수 출력", memberId, type);
-        return ApiResponse.onSuccess(postService.getTotalCountByMember(memberId,type));
+        return ApiResponse.onSuccess(postService.getTotalCountByMember(memberId, type));
     }
 
     @GetMapping("/count/by-member")
     public ApiResponse<?> getTotalCountByMember(
             @RequestParam PostType type,
-            @RequestParam String memberId
-    ) {
+            @RequestParam String memberId) {
         log.info("{}의 {} 개수 출력", memberId, type);
-        return ApiResponse.onSuccess(postService.getTotalCountByMember(memberId,type));
+        return ApiResponse.onSuccess(postService.getTotalCountByMember(memberId, type));
     }
 
     private String getLoginMemberId() {
         return SecurityUtil.getLoginMemberId().orElseThrow(() -> new ErrorHandler(ErrorStatus.MEMBER_NOT_FOUND));
     }
-
-
-
 }
