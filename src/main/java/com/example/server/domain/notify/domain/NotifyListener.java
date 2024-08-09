f1a098bd4a28f0ae44b23683039b1b33406ac4df
dh1010a <dh1010a@naver.com>
2024-08-09 10:31:53 +0900

    fix: fcm 키 없으면 요청 보내지 않도록 로직 변경

--- src/main/java/com/example/server/domain/notify/domain/NotifyListener.java
+++ src/main/java/com/example/server/domain/notify/domain/NotifyListener.java
@@ -2,6 +2,7 @@ package com.example.server.domain.notify.domain;
 
 import com.example.server.domain.notify.dto.NotifyDto;
 import com.example.server.domain.notify.service.FCMNotifyService;
+import com.example.server.domain.notify.service.FCMTokenService;
 import com.example.server.domain.notify.service.NotifyService;
 import com.example.server.domain.notify.service.SseNotifyService;
 import com.google.firebase.messaging.Message;
@@ -22,12 +23,16 @@ public class NotifyListener {
     private final SseNotifyService sseNotifyService;
     private final NotifyService notifyService;
     private final FCMNotifyService fcmNotifyService;
+    private final FCMTokenService fcmTokenService;
 
     @TransactionalEventListener
     @Async
     public void handleSseNotification(NotifyDto.NotifyPublishRequestDto requestDto) {
         sseNotifyService.sendNotify(requestDto.getReceiver(), requestDto);
-        sendFCMNotification(requestDto.getReceiver().getMemberId(), fcmNotifyService.createFCMMessage(requestDto.getReceiver(), requestDto));
+        if (fcmTokenService.getFCMToken(requestDto.getReceiver().getMemberId()) != null) {
+            sendFCMNotificationAsync(requestDto.getReceiver().getMemberId(), fcmNotifyService.createFCMMessage(requestDto.getReceiver(), requestDto));
+        }
+//        sendFCMNotification(requestDto.getReceiver().getMemberId(), fcmNotifyService.createFCMMessage(requestDto.getReceiver(), requestDto));
     }
 
 //    @TransactionalEventListener(phase = AFTER_COMMIT)
@@ -39,7 +44,11 @@ public class NotifyListener {
 
 
     private void sendFCMNotification(String memberId, Message message) {
-        fcmNotifyService.sendFCMNotification(memberId, message);
+        try {
+            fcmNotifyService.sendFCMNotification(memberId, message);
+        } catch (Exception e) {
+            log.error("Failed to send FCM Notification. MemberId: [{}], Message: [{}]", memberId, message);
+        }
     }
 
     private void sendFCMNotificationAsync(String memberId, Message message) {
