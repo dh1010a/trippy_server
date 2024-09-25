b3fcae389801f167de7f412336185a60accff819
dh1010a <dh1010a@naver.com>
2024-09-25 23:52:01 +0900

    fix: 깃 에러 수정

--- src/main/java/com/example/server/global/auth/security/handler/LogoutSuccessHandler.java
+++ src/main/java/com/example/server/global/auth/security/handler/LogoutSuccessHandler.java
@@ -1,4 +1,4 @@
-package com.example.server.global.auth.security.handler;
-
-public class LogoutSuccessHandler {
-}
+//package com.example.server.global.auth.security.handler;
+//
+//public class LogoutSuccessHandler {
+//}
